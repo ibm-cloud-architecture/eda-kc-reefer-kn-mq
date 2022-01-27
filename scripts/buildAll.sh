2fe495e089dc97346d5a68d304f14ef3e81d4a89
Jerome Boyer <jerome.boyer@gmail.com>
2022-01-27 14:35:27 -0800

    change to app name

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 scriptDir=$(dirname $0)
 
-IMAGE_NAME=quay.io/ibmcase/eda-kc-reefer-mq
+IMAGE_NAME=quay.io/ibmcase/eda-kc-reefer-ms-mq
 ./mvnw clean package -DskipTests -Dquarkus.container-image.build=false -Dquarkus.container-image.deploy=false
 docker build -f src/main/docker/Dockerfile.jvm -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
